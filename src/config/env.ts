a7b71d60034656a2ffd96da91314c128d7052829
vtarun <vivektarun89@gmail.com>
2026-02-21 23:12:54 +0530

    Fix .env file import issue

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,17 @@
+import { AppError } from "../utils/appError";
+
 export type NodeDev = 'development' | 'test' | 'production';
+
+function requiredENV(name: string){
+    const val = process.env[name];
+    if(!val){        
+        throw new AppError(`${name} is required`, 500);        
+    }
+    return val;
+}
 
 export const PORT = Number(process.env.PORT) || 3000;
 export const NODE_ENV: NodeDev = (process.env.NODE_ENV as NodeDev) || 'development';
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/doctor_app';
-export const JWT_SECRET = process.env.JWT_SECRET!
-export const BCRYPT_SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS || 12;
+export const MONGO_URI = requiredENV('MONGO_URI');
+export const JWT_SECRET = requiredENV('JWT_SECRET');
+export const BCRYPT_SALT_ROUNDS = requiredENV('BCRYPT_SALT_ROUNDS');
